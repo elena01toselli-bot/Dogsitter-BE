fd30f42bafc422f547299ec5ddc0b49de52446fc
alberto04-savi <alberto.bagnolini@edu.unife.it>
2026-05-27 17:31:14 +0200

    Delete src directory

--- src/app/modelli/recensione.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Recensione {
-  usernameCliente: string;       // FK → CLIENTE
-  usernameDogsitter: string;     // FK → DOG_SITTER
-  voto: number;
-  commento: string;
-  data: string;
-}
